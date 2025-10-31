104d9790b3911ca94d1d8b943f521984f186dc34
Jahidsheikh09 <pixmjahid9428@gmail.com>
2025-11-01 04:36:58 +0530

    last final

--- src/utils/cryptoUtils.js
+++ src/utils/cryptoUtils.js
@@ -6,7 +6,7 @@ export const decryptMessage = (cipherText) => {
   try {
     const bytes = CryptoJS.AES.decrypt(cipherText, MESSAGE_SECRET_KEY);
     const originalText = bytes.toString(CryptoJS.enc.Utf8);
-    return originalText || "[Decryption Failed]";
+    return originalText;
   } catch (error) {
     console.error("Error decrypting message:", error);
     return "[Invalid Encrypted Data]";
